b74c0a0f03804418931cd0cca5aebe7651b3707d
BekerDi <d.beker.04@mail.ru>
2023-10-04 22:21:59 +0400

    Добавьте файлы проекта.

---
+++ dz(4)/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace dz_4_
+{
+    internal class Program
+    {
+        enum GrumpinessLevel
+        {
+            Low,
+            Medium,
+            High
+        }
+
+        struct Grandpa
+        {
+            public string Name;
+            public GrumpinessLevel Level;
+            public string[] GrumpyPhrases;
+            public int Bruises;
+
+            public Grandpa(string name, GrumpinessLevel level, string[] grumpyPhrases)
+            {
+                Name = name;
+                Level = level;
+                GrumpyPhrases = grumpyPhrases;
+                Bruises = 0;
+            }
+
+            public int CountBruises(params string[] swearWords)
+            {
+                int count = 0;
+
+                foreach (string word in swearWords)
+                {
+                    foreach (string phrase in GrumpyPhrases)
+                    {
+                        if (phrase.Contains(word))
+                        {
+                            count++;
+                            break;
+                        }
+                    }
+                }
+
+                Bruises += count;
+                return count;
+            }
+        }
+        static int Calculate(ref int product, out double srznc, params int[] numbers)
+        {
+            int sum = 0;
+            product = 1;
+            srznc = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+                product *= number;
+            }
+            srznc = (double)sum / numbers.Length;
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1");
+            int[] numbers = new int[20];
+            Random rm = new Random();
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rm.Next(1, 1000);
+
+
+                Console.WriteLine(numbers[i] + " ");
+            }
+            Console.WriteLine("Введите индекс первого числа");
+            bool fl = int.TryParse(Console.ReadLine(), out int index1);
+            Console.WriteLine("Введите индекс второго числа");
+            bool fl1 = int.TryParse(Console.ReadLine(), out int index2);
+            if (index1 < 0 || index1 > numbers.Length || index2 < 0 || index2 > numbers.Length)
+            {
+                Console.WriteLine("Вы ввели значение находящееся вне интервала от 0 до 20");
+
+            }
+            else
+            {
+                int temp;
+                temp = numbers[index1];
+                numbers[index1] = numbers[index2];
+                numbers[index2] = temp;
+                Console.WriteLine("Итоговый массив");
+                for (int i = 0; i < numbers.Length; i++)
+                {
+
+
+                    Console.WriteLine(numbers[i] + "");
+                }
+
+
+            }
+            Console.WriteLine("Задание 2");
+
+
+            Console.WriteLine("Задание 3");
+            Console.WriteLine("Введите число от 0 до 9");
+            bool fl2 = int.TryParse(Console.ReadLine(), out int num);
+            if (num < 0 || num > 9)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка!");
+                Thread.Sleep(3000);
+                Console.ForegroundColor = ConsoleColor.Black;
+
+            }
+            else
+            {
+                Console.WriteLine();
+                if (num == 0)
+                {
+                    Console.WriteLine(" ### ");
+                    Console.WriteLine(" # # ");
+                    Console.WriteLine(" # # ");
+                    Console.WriteLine(" ### ");
+                }
+                if (num == 1)
+                {
+                    Console.WriteLine("  #  ");
+                    Console.WriteLine(" ##  ");
+                    Console.WriteLine("  #  ");
+                    Console.WriteLine(" ### ");
+                }
+                if (num == 2)
+                {
+                    Console.WriteLine("  ### ");
+                    Console.WriteLine(" #  # ");
+                    Console.WriteLine("   #  ");
+                    Console.WriteLine("  ### ");
+                }
+                if (num == 3)
+                {
+                    Console.WriteLine(" ### ");
+                    Console.WriteLine(" #   ");
+                    Console.WriteLine(" ###  ");
+                    Console.WriteLine(" #   ");
+                    Console.WriteLine(" ### ");
+                }
+                if (num == 4)
+                {
+                    Console.WriteLine(" # # ");
+                    Console.WriteLine(" ### ");
+                    Console.WriteLine("   # ");
+                    Console.WriteLine("   # ");
+                }
+                if (num == 5)
+                {
+                    Console.WriteLine(" ### ");
+                    Console.WriteLine(" #   ");
+                    Console.WriteLine(" ### ");
+                    Console.WriteLine("   # ");
+                    Console.WriteLine(" ### ");
+                }
+                if (num == 6)
+                {
+                    Console.WriteLine(" ### ");
+                    Console.WriteLine(" #   ");
+                    Console.WriteLine(" ### ");
+                    Console.WriteLine(" # # ");
+                    Console.WriteLine(" ### ");
+                }
+                if (num == 7)
+                {
+                    Console.WriteLine(" ### ");
+                    Console.WriteLine("   # ");
+                    Console.WriteLine("   # ");
+                    Console.WriteLine("   # ");
+                }
+                if (num == 8)
+                {
+                    Console.WriteLine(" ### ");
+                    Console.WriteLine(" # # ");
+                    Console.WriteLine(" ### ");
+                    Console.WriteLine(" # # ");
+                    Console.WriteLine(" ### ");
+                }
+                if (num == 9)
+                {
+                    Console.WriteLine(" ### ");
+                    Console.WriteLine(" # # ");
+                    Console.WriteLine(" ### ");
+                    Console.WriteLine("   # ");
+                    Console.WriteLine(" ### ");
+                }
+
+            }
+            Console.WriteLine("Задание 4");
+            Grandpa grandpa1 = new Grandpa("Денис", GrumpinessLevel.Low, new string[] { "Черт", "Дурак", "Простофиля",  });
+            Grandpa grandpa2 = new Grandpa("Леша", GrumpinessLevel.Medium, new string[] { "Черт", "Лентяй" });
+            Grandpa grandpa3 = new Grandpa("Игорь", GrumpinessLevel.High, new string[] { "Дурак", "Глупец", "Дура", "Хобяка", "Чудак!", "Насупа"  });
+            Grandpa grandpa4 = new Grandpa("Саша", GrumpinessLevel.Medium, new string[] { "Черт веревочный", "Туес", "Пресноплюй", "Насупа", "Ендовочник" });
+            Grandpa grandpa5 = new Grandpa("Герман", GrumpinessLevel.Low, new string[] { "Лентяй" });
+
+            List<Grandpa> grandpas = new List<Grandpa> { grandpa1, grandpa2, grandpa3, grandpa4, grandpa5 };
+
+            List<string> swearWords = new List<string> { "Черт", "Чудак", "Дура", "Дурак", "Простофиля", "Лентяй", "Глупец", "Хобяка", "Туес", "Пресноплюй", "Насупа", "Ендовочник" };
+
+            foreach (Grandpa grandpa in grandpas)
+            {
+                int bruises = grandpa.CountBruises(swearWords.ToArray());
+                Console.WriteLine($"{grandpa.Name} получил {bruises} фингал.");
+            }
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
+    
+
---
+++ dz_tumakov5/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz4
+{
+    internal class Program
+    {
+        /// Для задания 1
+        static int FindMax(int num1, int num2)
+        {
+            int find = Math.Max(num1, num2);
+            return find;
+        }
+        static int Zamena(ref int n1, ref int n2)
+        {
+            int n3 = n1;
+            n1 = n2;
+            n2 = n3;
+            return n1;
+            return n2;
+
+        }
+        static bool FindFactorial(int numb, out int result)
+        {
+            try
+            {
+                checked
+                {
+                    int factorial = 1;
+
+
+                    for (int i = 1; i < numb; i++)
+                    {
+                        factorial *= i;
+
+                    }
+                    result = factorial;
+                    return true;
+                }
+            }
+            catch (OverflowException)
+            {
+                result = 0;
+                return false;
+            }
+        }
+        static int Factorial(int i)
+        {
+            if (i == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return i * Factorial(i - 1);
+            }
+        }
+        static int NOD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+        static int NODfor3(int a, int b, int c)
+        {
+            {
+                return NOD(NOD(a, b), c);
+            }
+
+
+        }
+        static int Fibonacci(int f)
+        {
+            if (f <= 1)
+            {
+                return f;
+            }
+            return Fibonacci(f - 1) + Fibonacci(f - 2);
+        }
+
+
+
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Задание 1");
+            Console.WriteLine("Введите первое число");
+            bool a = Int32.TryParse(Console.ReadLine(), out int num1);
+            Console.WriteLine("Введите второе число и нажмите ENTER, чтобы увидеть результат");
+            bool b = Int32.TryParse(Console.ReadLine(), out int num2);
+            int num = FindMax(num1, num2);
+            Console.WriteLine("Большее число " + num);
+
+            Console.WriteLine("Задание 2");
+
+
+            Console.WriteLine("ведите значение первого параметра");
+            bool p1 = Int32.TryParse(Console.ReadLine(), out int n1);
+            Console.WriteLine("Введите значение второго параметра и нажмите ENTER, чтобы увидеть результат");
+            bool p2 = Int32.TryParse(Console.ReadLine(), out int n2);
+            Console.WriteLine("Вы ввели параметры: x = " + n1 + ", y=  " + n2);
+            Zamena(ref n1, ref n2);
+            Console.WriteLine("Значение после обмена: х= " + n1 + " , y= " + n2);
+
+
+
+            Console.WriteLine("Вы ввели нечисловое значение");
+
+
+            Console.WriteLine("Задание 5.3");
+            bool fl = true;
+
+
+            Console.WriteLine("Введите целое число");
+            bool suc = Int32.TryParse(Console.ReadLine(), out int number);
+            if (suc)
+            {
+                bool result = FindFactorial(number, out int factorial);
+                if (result)
+                {
+                    Console.WriteLine(number + "!" + " = " + factorial);
+                }
+                else
+                {
+                    Console.WriteLine("Переполнение при вычислении  " + number + "!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели нечисловое или нецелочисленное значение.");
+            }
+
+
+
+            Console.WriteLine("Задание 5.4");
+
+            Console.WriteLine("Введите целое число");
+            bool c = int.TryParse(Console.ReadLine(), out int fact);
+            int znach = Factorial(fact);
+            Console.WriteLine(fact + "!" + " = " + znach);
+
+            Console.WriteLine("Задание 5.1");
+            Console.WriteLine("Введите два числа для вычисления НОДа");
+            bool ch1 = Int32.TryParse(Console.ReadLine(), out int x);
+            bool ch2 = Int32.TryParse(Console.ReadLine(), out int y);
+            bool ch3 = Int32.TryParse(Console.ReadLine(), out int u);
+            Console.WriteLine("НОД чисел {0}, {1} = {2}", x, y, NOD(x, y));
+            Console.WriteLine("НОД чисел {0}, {1}, {2}= {3}", x, y, u, NOD(NOD(x, y), u));
+
+            Console.WriteLine("Задание 5.2");
+            Console.WriteLine("Введите номер числа ряда Фибоначчи");
+            int numf = int.Parse(Console.ReadLine());
+            int itog = Fibonacci(numf);
+            Console.WriteLine("Значение " + numf + " числа ряда Фибоначчи: " + itog);
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
